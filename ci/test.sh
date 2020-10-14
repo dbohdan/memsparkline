7429d48a5fe2ea0c376caaa6022062c92b5535d4
D. Bohdan <dbohdan@dbohdan.com>
2020-10-14 13:08:54 +0000

    feat(cli): implement option --wait

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,8 @@
 set -e
 
 cd "$(dirname "$0")"
+
 ../memsparkline sleep 1 2>&1 | grep max > /dev/null
+
+../memsparkline -w 2000 sleep 1 2>&1 | wc -l | grep '^3$' > /dev/null
+../memsparkline -n -w 100 sleep 1 2>&1 | wc -l | grep '^11$' > /dev/null
