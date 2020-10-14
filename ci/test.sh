8af9dad5f5458659c7d8aa82cdabfa045e8cb668
D. Bohdan <dbohdan@dbohdan.com>
2020-10-14 13:15:08 +0000

    test(wait): fix expected number of lines

--- ci/test.sh
+++ ci/test.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 ../memsparkline sleep 1 2>&1 | grep max > /dev/null
 
 ../memsparkline -w 2000 sleep 1 2>&1 | wc -l | grep '^3$' > /dev/null
-../memsparkline -n -w 100 sleep 1 2>&1 | wc -l | grep '^11$' > /dev/null
+../memsparkline -n -w 100 sleep 1 2>&1 | wc -l | grep '^1[0-9]$' > /dev/null
