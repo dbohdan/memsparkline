fcdeb12f7254bc3ec6bc9698e06584f69003a587
D. Bohdan <dbohdan@dbohdan.com>
2020-10-14 13:20:03 +0000

    test(*): make tests not silent

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-../memsparkline sleep 1 2>&1 | grep max > /dev/null
+../memsparkline sleep 1 2>&1 | grep max
 
-../memsparkline -w 2000 sleep 1 2>&1 | wc -l | grep '^3$' > /dev/null
-../memsparkline -n -w 100 sleep 1 2>&1 | wc -l | grep '^1[0-9]$' > /dev/null
+../memsparkline -w 2000 sleep 1 2>&1 | wc -l | grep '^3$'
+../memsparkline -n -w 100 sleep 1 2>&1 | wc -l | grep '^1[0-9]$'
