99dba15e764e061c527363fa80b3f30aa0de705c
D. Bohdan <dbohdan@dbohdan.com>
2023-07-04 20:16:12 +0000

    ci(gitlab): run `poetry install`

--- ci/test.sh
+++ ci/test.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")"/../ || exit 1
 
 status=0
 printf '=== mypy\n'
+poetry install --quiet
 poetry run mypy memsparkline || status=1
 printf '=== Tests\n'
 python3 tests/test_memsparkline.py || status=1
