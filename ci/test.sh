0d8bc24ffe0143a78b210074d984973c765fac9e
D. Bohdan <dbohdan@dbohdan.com>
2023-07-04 19:56:52 +0000

    ci(gitlab): run mypy check

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
-set -e
 
-python3 "$(dirname "$0")/../tests/test_memsparkline.py"
+cd "$(dirname "$0")"/../ || exit 1
+
+status=0
+printf '=== mypy\n'
+poetry run mypy memsparkline || status=1
+printf '=== Tests\n'
+python3 tests/test_memsparkline.py || status=1
+exit "$status"
