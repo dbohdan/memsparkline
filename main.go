9d9442cf55ada6a53324fe96192e58d7a7aab33d
D. Bohdan <dbohdan@dbohdan.com>
2024-12-31 10:41:27 +0000

    build(version): bump to 0.9.4

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ const (
 	defaultWait         = -1
 	sparklineLowMaximum = 10000
 	usageDivisor        = 1 << 20 // Report memory usage in binary megabytes.
-	version             = "0.9.3"
+	version             = "0.9.4"
 )
 
 var sparklineTicks = []rune{'▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'}
